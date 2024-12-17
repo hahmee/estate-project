dc65f1fdfb0db9485b12083a10130a7bd867bcc1
HYE <hamye4143@gmail.com>
2024-12-17 23:15:48 +0900

    Clear cache and apply .gitignore

--- client/src/util/formatting.js
+++
@@ -1,8 +0,0 @@
-//화폐 형식 변환기
-export const currencyFormatter = new Intl.NumberFormat('ko-KR', {
-    style: 'currency',
-    currency: 'krw',
-});
-
-//개수 형식 변환기
-export const numberFormatter = new Intl.NumberFormat('ko-KR');
