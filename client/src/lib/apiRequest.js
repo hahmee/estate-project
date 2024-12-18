f5dc69df72831cbd528414885ac9e082b4466cbb
HYE <hamye4143@gmail.com>
2024-12-18 16:32:24 +0900

    Change apiRequest

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  // baseURL: "http://ec2-13-124-226-52.ap-northeast-2.compute.amazonaws.com:8800/api", //8800
-  baseURL: "http://localhost:8800/api",
-  // baseURL: '/api', // nginx에서 /api/로 프록시 설정
+  baseURL: process.env.VITE_API_REQUEST,
   withCredentials: true,
 });
 
